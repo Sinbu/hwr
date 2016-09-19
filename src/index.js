a3ab6eefa39600a52924b59e487e21c9005b99f0
Sina Yeganeh <sinayega@gmail.com>
2016-09-18 23:58:40 -0700

    initial commit after boiler

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Detail from './pages/Detail';
+
+ReactDOM.render(
+        <Detail message="This is props"/>,
+        document.getElementById('app')
+        );
